9a7c1e63e75f8e40316b979abdffc49ca9a2dcf1
yousaf wazir <umbrellaman612@gmail.com>
2022-07-24 18:09:49 +0100

    feat: added redirect

---
+++ src/pages/protected.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { getSession } from 'next-auth/react'
+const notHittable: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>protected</title>
+      </Head>
+      protected route
+    </div>
+  )
+}
+export default notHittable
+
+export async function getServerSideProps(ctx: any) {
+  const session = await getSession(ctx)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: { session },
+  }
+}
