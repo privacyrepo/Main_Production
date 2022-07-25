6d59180938c80a4107981da280ecda7b77a02ce5
yousaf wazir <umbrellaman612@gmail.com>
2022-07-25 17:44:50 +0100

    feat: added loaders

---
+++ src/pages/feats/react-loader.tsx
@@ -0,0 +1,33 @@
+import { CSSProperties, useState } from 'react'
+import ClipLoader from 'react-spinners/ClipLoader'
+
+const override: CSSProperties = {
+  display: 'block',
+  margin: '0 auto',
+  borderColor: 'red',
+}
+
+function App() {
+  let [loading, setLoading] = useState(true)
+  let [color, setColor] = useState('#ffffff')
+
+  return (
+    <div className="sweet-loading">
+      <button onClick={() => setLoading(!loading)}>Toggle Loader</button>
+      <input
+        value={color}
+        onChange={(input) => setColor(input.target.value)}
+        placeholder="Color of the loader"
+      />
+
+      <ClipLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={150}
+      />
+    </div>
+  )
+}
+
+export default App
